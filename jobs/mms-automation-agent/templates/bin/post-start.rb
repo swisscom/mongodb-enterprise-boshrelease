e6f384eff81578db00d330f4f89457540dcc101f
taapekr1 <kristjan.perlaska@swisscom.com>
2022-08-29 15:43:08 +0200

    Adds Resync wait

---
+++ jobs/mms-automation-agent/templates/bin/post-start.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'json'
+
+def cluster_healty?
+
+  str = `mongo localhost:<%= p('mongodb.port') %>/admin -u <%= p('mongodb.health.user') %> -p '<%= p('mongodb.health.password') %>' --eval 'JSON.stringify(rs.status())' --quiet`
+  puts str
+
+  parsed = JSON.parse(str)
+
+  if parsed['errmsg'] == 'not running with --replSet'
+    puts 'not in cluster mode, proceeding'
+    `/var/vcap/bosh/bin/monit stop mms-automation-agent`
+    `pkill mongo`
+    exit 0
+  end
+
+  cluster_healty = true
+
+  parsed['members'].each { |member|
+    if member['health'] == 0
+      puts 'unhealthy node: ' + member['name']
+      cluster_healty = false
+    end
+
+  }
+  cluster_healty
+end
+
+
+# wait up to 10 minutes for cluster to get healty
+while true do
+  if cluster_healty?
+    puts 'cluster OK, proceeding'
+    break 
+  else
+    puts 'cluster unhealty; wait and try again'
+    sleep 10
+  end
+end
