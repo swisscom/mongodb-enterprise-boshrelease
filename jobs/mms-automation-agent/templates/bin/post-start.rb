14a7ffb94cd68b7fdabbd5afd928e283f04a9efd
taapekr1 <kristjan.perlaska@swisscom.com>
2022-10-03 18:10:38 +0200

    Adds faulty mongoschell detect and retries

--- jobs/mms-automation-agent/templates/bin/post-start.rb
+++ jobs/mms-automation-agent/templates/bin/post-start.rb
@@ -6,12 +6,25 @@ def cluster_healty?
   begin
 
     str = `mongo localhost:<%= p('mongodb.port') %>/admin -u <%= p('mongodb.health.user') %> -p '<%= p('mongodb.health.password') %>' --eval 'JSON.stringify(rs.status())' --quiet`
-    puts str
+
+    # Raise Error if status code is not 0s
+    if $? != 0
+      raise RuntimeError.new("Error connecting to mongodb")
+    end
+
     parsed = JSON.parse(str)
 
-  rescue JSON::ParserError
-
+  # Rescue mongoshell error 5 times
+  rescue RuntimeError => e
     sleep 5
+    try += 1
+    puts e
+    try <= 5 ? retry : raise
+
+  # Rescue jsonparse error forever
+  rescue JSON::ParserError => e
+    sleep 5
+    puts e
     retry
 
   end
