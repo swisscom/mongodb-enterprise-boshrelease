c6d41b0706fa7bf8401c9d7d82e5c1e6ba64960a
Kristjan Perlaska <kristjan.perlaska@swisscom.com>
2022-10-04 15:01:09 +0200

    Fix post start (#13)
    * Adds faulty mongoschell detect and retries
    
    * Adds more states to detection
    
    * Adds timeout and skipfile
    
    * Increasing timeout
    
    * changing if order

--- jobs/mms-automation-agent/templates/bin/post-start.rb
+++ jobs/mms-automation-agent/templates/bin/post-start.rb
@@ -1,17 +1,34 @@
 require 'rubygems'
 require 'json'
 
+skipFile="/var/vcap/store/mongodb-skip-post-start"
+startTime = Time.now
+timeOut = 28800
+
 def cluster_healty?
 
   begin
 
     str = `mongo localhost:<%= p('mongodb.port') %>/admin -u <%= p('mongodb.health.user') %> -p '<%= p('mongodb.health.password') %>' --eval 'JSON.stringify(rs.status())' --quiet`
-    puts str
+
+    # Raise Error if status code is not 0s
+    if $? != 0
+      raise RuntimeError.new("Error connecting to mongodb")
+    end
+
     parsed = JSON.parse(str)
 
-  rescue JSON::ParserError
+  # Rescue mongoshell error 5 times
+  rescue RuntimeError => e
+    sleep 10
+    try += 1
+    puts e
+    try <= 12 ? retry : raise
 
+  # Rescue jsonparse error forever
+  rescue JSON::ParserError => e
     sleep 5
+    puts e
     retry
 
   end
@@ -20,8 +37,10 @@ def cluster_healty?
   cluster_healty = true
 
   parsed['members'].each { |member|
-    if member['health'] == 0
-      puts 'unhealthy node: ' + member['name']
+    memberHealth=member['health']
+    # 0 => STARTUP, 3 => RECOVERING, 5 => STARTUP2, 9 => ROLLBACK
+    if memberHealth == 0 || memberHealth == 3 || memberHealth == 5 || memberHealth == 9
+      puts "unhealthy node: #{member['name']} with node status: #{memberHealth}"
       cluster_healty = false
     end
 
@@ -29,14 +48,18 @@ def cluster_healty?
   cluster_healty
 end
 
-
-# wait until cluster is healthy 
-while true do
-  if cluster_healty?
-    puts 'cluster OK, proceeding'
-    break 
+# wait until cluster is healthy until timeout is reached or skipfile exists
+# if cluster is healthy and skipfile exsists it will be healthy and ignore skipfile
+while Time.now < startTime + timeOut  do
+  if File.file?(skipFile)
+    puts "#{skipFile} found, therefore skipping"
+    break
+  elsif cluster_healty?
+    puts "cluster OK, proceeding"
+    break
   else
-    puts 'cluster unhealty; wait and try again'
+    puts "cluster unhealty; wait and try again"
     sleep 10
+
   end
 end
